37150e8394ec38b6c232f5305590edb8d4e8ff13
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-02-21 20:11:12 -0300

    feat: estruturando footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,39 @@
 const Footer = () => {
     return (
         <>
+            <section>
+                <div>
+                    <div>
+                        <h3>Institucional</h3>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Sobre</a></li>
+                            <li><a href="">Produtos</a></li>
+                            <li><a href="">Perguntas frequentes</a></li>
+                            <li><a href="">Fale conosco</a></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>Ajuda</h3>
+                        <ul>
+                            <li><a href="#">Trocas e devoluções</a></li>
+                            <li><a href="">Termos e condições</a></li>
+                            <li><a href="">Política de privacidade</a></li>
+                            <li><a href="">Padrão de qualidade</a></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>Endereço</h3>
+                        <p>Rua Vale do Silício, 321</p>
+                        <p>São Francisco, Califórnia</p>
+                        <p>Estados Unidos da América</p>
+                        <p>(34) 1234.5678</p>
+                        <p><a href="#">vendas@ecommercedd.com</a></p>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,9 +1,14 @@
 import { Header } from "../../header"
+import { Footer } from "../../footer"
 
 const Home = () => {
     return (
         <>
             <Header />
+            <section>
+                <h1>Home</h1>
+            </section>
+            <Footer/>
         </>
     )
 }
